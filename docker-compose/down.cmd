dec98cd98825875840612d6a3ac4221ccbeec509
Benedikt Eggers <be@spiegelburg.net>
2022-06-05 12:44:51 +0200

    Add basic docker-compose structure

---
+++ docker-compose/down.cmd
@@ -0,0 +1,6 @@
+docker-compose -f docker-compose.yml ^
+               -f docker-compose.db.yml ^
+               -f docker-compose.message_broker.yml ^
+               -f docker-compose.monitoring.yml ^
+               -f docker-compose.services.yml ^
+               down
