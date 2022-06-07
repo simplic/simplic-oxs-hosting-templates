0a3eb5d50031613eae9ff7bad77fa2916328c9a8
Benedikt Eggers <be@spiegelburg.net>
2022-06-07 12:33:19 +0200

    Add custom

--- docker-compose/down.cmd
+++ docker-compose/down.cmd
@@ -4,4 +4,7 @@ docker-compose -f docker-compose.yml ^
                -f docker-compose.monitoring.yml ^
                -f docker-compose.services.yml ^
                -f docker-compose.frontend.yml ^
+               -f docker-compose.custom.yml ^
                down
+
+REM `docker-compose.custom.yml` must always be the last file, because it will override other settings
