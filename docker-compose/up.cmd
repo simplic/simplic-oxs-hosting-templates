ec33ff4e3712cb589c9f233fd729c26a0ec86655
Benedikt Eggers <be@spiegelburg.net>
2022-06-07 07:44:27 +0200

    Add simple hosting via docker-compose

--- docker-compose/up.cmd
+++ docker-compose/up.cmd
@@ -3,4 +3,4 @@ docker-compose -f docker-compose.yml ^
                -f docker-compose.message_broker.yml ^
                -f docker-compose.monitoring.yml ^
                -f docker-compose.services.yml ^
-               up -d
+               up
