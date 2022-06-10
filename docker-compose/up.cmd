1f168f1c5b2259a7a7fa90b0f9308f1361fe8885
Tobias <tobias.milde@simplic.biz>
2022-06-10 14:35:45 +0200

    Correct pull.

--- docker-compose/up.cmd
+++ docker-compose/up.cmd
@@ -1,6 +1,14 @@
 REM Pull all images with :latest or a specific tag
 if "%1" == "pull" (
-    docker-compose pull
+	docker-compose -f docker-compose.yml ^
+				   -f docker-compose.db.yml ^
+				   -f docker-compose.message_broker.yml ^
+				   -f docker-compose.monitoring.yml ^
+				   -f docker-compose.services.yml ^
+				   -f docker-compose.frontend.yml ^
+				   -f docker-compose.custom.yml ^
+				   pull
+
 )
 
 docker-compose -f docker-compose.yml ^
