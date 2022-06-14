2d70413d7a9425cb4c60ea8bc7ab21ae9f74a904
Benedikt Eggers <be@spiegelburg.net>
2022-06-14 12:20:25 +0200

    Add env templates

--- docker-compose/up.cmd
+++ docker-compose/up.cmd
@@ -1,6 +1,13 @@
 REM Pull all images with :latest or a specific tag
 if "%1" == "pull" (
-    docker-compose pull
+    docker-compose -f docker-compose.yml ^
+                   -f docker-compose.db.yml ^
+                   -f docker-compose.message_broker.yml ^
+                   -f docker-compose.monitoring.yml ^
+                   -f docker-compose.services.yml ^
+                   -f docker-compose.frontend.yml ^
+                   -f docker-compose.custom.yml ^
+                   pull
 )
 
 docker-compose -f docker-compose.yml ^
@@ -12,4 +19,4 @@ docker-compose -f docker-compose.yml ^
                -f docker-compose.custom.yml ^
                up
 
-REM `docker-compose.custom.yml` must always be the last file, because it will override other settings
+REM `docker-compose.custom.yml` must always be the last file, because it will override other settings   
